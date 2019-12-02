941e7829a3cae9a4831a0c38d4776183c4b848bd
AlexisKelly <Alexis.Kelly@bjss.co.uk>
2019-12-02 08:49:31 +0000

    First Task for Day 1 completed

---
+++ Day1/src/main/java/FuelRequirement.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class FuelRequirement {
+
+    public static void main(String args[]) throws FileNotFoundException {
+        task1();
+    }
+
+    public static void task1() throws FileNotFoundException {
+        File file = new File("../../../Input/input.txt");
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String mass = "";
+        int counter = 0;
+        int totalFuel =  0;
+
+        while (true) {
+            try {
+                if((mass = br.readLine()) !=null){
+                    counter++;
+                    int fuelRequired = ((int) Integer.parseInt(mass)/3)-2;
+
+                    totalFuel += fuelRequired;
+                    System.out.println(counter+" "+mass+" FuelRequired:"+fuelRequired);
+                } else {
+                    break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Total Fuel:"+totalFuel);
+    }
+
+}
