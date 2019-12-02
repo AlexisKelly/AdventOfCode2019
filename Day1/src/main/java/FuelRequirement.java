5e8e8f4d43c5033b6c3d8f770540e6bfdca5a4fa
AlexisKelly <Alexis.Kelly@bjss.co.uk>
2019-12-02 18:40:19 +0000

    Task 2 completed

--- Day1/src/main/java/FuelRequirement.java
+++ Day1/src/main/java/FuelRequirement.java
@@ -14,12 +14,15 @@ public class FuelRequirement {
         String mass = "";
         int counter = 0;
         int totalFuel =  0;
+        int task2FuelRequirements = 0;
 
         while (true) {
             try {
                 if((mass = br.readLine()) !=null){
                     counter++;
-                    int fuelRequired = ((int) Integer.parseInt(mass)/3)-2;
+                    int fuelRequired = getFuel(Integer.parseInt(mass));
+
+                    task2FuelRequirements+= task2(fuelRequired);
 
                     totalFuel += fuelRequired;
                     System.out.println(counter+" "+mass+" FuelRequired:"+fuelRequired);
@@ -31,6 +34,32 @@ public class FuelRequirement {
             }
         }
         System.out.println("Total Fuel:"+totalFuel);
+
+        System.out.println("Task 2 Total:"+task2FuelRequirements);
+
+    }
+
+    public static int task2(int totalFuel){
+
+        int fuelRequirements = totalFuel;
+        while (totalFuel > 0){
+
+            totalFuel = getFuel(totalFuel);
+            if(totalFuel > 0){
+                fuelRequirements+= totalFuel;
+                System.out.println("total fuel remaining:"+totalFuel);
+            }
+
+        }
+        System.out.println("FuelRequirements:"+fuelRequirements);
+
+        return fuelRequirements;
+
+    }
+
+    public static int getFuel(int mass){
+
+        return ((int) mass /3)-2;
     }
 
 }
